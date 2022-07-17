073880dcae3ab2cafd0de949052caa8581877c86
Tuanlt28 <lttuan28112002@gmail.com>
2022-07-18 00:24:55 +0700

    commit

---
+++ Web_API/Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_API.Data
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options) {}
+
+        
+    }
+}
